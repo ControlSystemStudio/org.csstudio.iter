8edf162ffcc5ffc50b90f9cec555d21ca20f6a11
utzeln <nadine.utzel@iter.org>
2015-06-17 08:31:53 +0000

    navigation folder for Demo and Design_Guide

---
+++ products/org.csstudio.iter.css.product/resources/templates/scripts/AlarmShortFormat.js
@@ -0,0 +1,20 @@
+/*
+Copyright (c) : 2010-2015 ITER Organization,
+CS 90 046
+13067 St. Paul-lez-Durance Cedex
+France
+ 
+This product is part of ITER CODAC software.
+For the terms and conditions of redistribution or use of this software
+refer to the file ITER-LICENSE.TXT located in the top level directory
+of the distribution package.
+*/
+
+
+importPackage(Packages.org.csstudio.opibuilder.scriptUtil);
+
+var t = new Date(PVUtil.getTimeInMilliseconds(pvs[0])).toTimeString();
+var st = t.split(" ");
+
+widget.setPropertyValue("on_label", st[0]);
+widget.setPropertyValue("off_label", st[0]);
