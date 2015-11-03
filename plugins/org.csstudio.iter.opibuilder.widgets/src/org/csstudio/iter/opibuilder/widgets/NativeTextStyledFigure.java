2289e46bdc98dffb248dc2348bd591c4ca68ca45
Boris Versic <boris.versic@cosylab.com>
2015-11-03 10:24:47 +0100

    .../opibuilder/widgets/NativeTextStyledFigure.java | 13 +++++  .../opibuilder/widgets/TextInputStyledModel.java   | 61 ++++++++++++++++++++++  products/org.csstudio.iter.css.product/plugin.xml  |  8 +--  .../plugin_customization.ini                       |  2 +-  4 files changed, 79 insertions(+), 5 deletions(-)

---
+++ plugins/org.csstudio.iter.opibuilder.widgets/src/org/csstudio/iter/opibuilder/widgets/NativeTextStyledFigure.java
@@ -0,0 +1,13 @@
+package org.csstudio.iter.opibuilder.widgets;
+
+import org.csstudio.opibuilder.editparts.AbstractBaseEditPart;
+import org.csstudio.opibuilder.widgets.figures.NativeTextFigure;
+
+public class NativeTextStyledFigure extends NativeTextFigure {
+
+	public NativeTextStyledFigure(AbstractBaseEditPart editPart, int style) {
+		super(editPart, style);
+		// TODO Auto-generated constructor stub
+	}
+
+}
--- plugins/org.csstudio.iter.opibuilder.widgets/src/org/csstudio/iter/opibuilder/widgets/TextInputStyledModel.java
+++ plugins/org.csstudio.iter.opibuilder.widgets/src/org/csstudio/iter/opibuilder/widgets/TextInputStyledModel.java
@@ -2,9 +2,12 @@ package org.csstudio.iter.opibuilder.widgets;
 
 import org.csstudio.opibuilder.properties.BooleanProperty;
 import org.csstudio.opibuilder.properties.ColorProperty;
+import org.csstudio.opibuilder.properties.ComboProperty;
+import org.csstudio.opibuilder.properties.StringProperty;
 import org.csstudio.opibuilder.properties.WidgetPropertyCategory;
 import org.csstudio.opibuilder.util.MediaService;
 import org.csstudio.opibuilder.widgets.model.TextInputModel;
+import org.csstudio.opibuilder.widgets.model.TextInputModel.FOCUS_TRAVERSE;
 import org.eclipse.swt.graphics.RGB;
 
 /**
@@ -28,6 +31,41 @@ public class TextInputStyledModel extends TextInputModel {
     /** Confirm (store value) at focus lost */
     public static final String PROP_CONFIRM_FOCUS_LOST = "confirm_focus_lost"; //$NON-NLS-1$
     
+    /** Text of the (optional) label for this input field */
+    public static final String PROP_INPUT_LABEL_TEXT = "input_label_text"; //$NON-NLS-1$
+    
+    /** Setting for the label's placement.
+     *  Determines whether the label should be stacked vertically (above) or horizontally (on the left) */
+    public static final String PROP_INPUT_LABEL_STACKING = "input_label_stacking"; //$NON-NLS-1$
+    
+    public enum INPUT_LABEL_STACKING {
+        VERTICAL("Vertical (above)"),
+        HORIZONTAL("Horizontal (beside on left)");
+
+        private String description;
+        private INPUT_LABEL_STACKING(String description) {
+            this.description = description;
+        }
+
+        @Override
+        public String toString() {
+            return description;
+        }
+
+        public static String[] stringValues(){
+            String[] result = new String[values().length];
+            int i =0 ;
+            for(INPUT_LABEL_STACKING f : values()){
+                result[i++] = f.toString();
+            }
+            return result;
+        }
+        
+        public static int getDefault() {
+        	return VERTICAL.ordinal();
+        };
+    } 
+    
 	public TextInputStyledModel() {
 	}
 
@@ -40,6 +78,13 @@ public class TextInputStyledModel extends TextInputModel {
 
         addProperty(new BooleanProperty(PROP_CONFIRM_FOCUS_LOST, "Confirm on Focus Lost",
                 WidgetPropertyCategory.Behavior, true));
+        
+        addProperty(new StringProperty(PROP_INPUT_LABEL_TEXT, "Label Text",
+                WidgetPropertyCategory.Display, "", false));
+
+        addProperty(new ComboProperty(PROP_INPUT_LABEL_STACKING, "Label Stacking mode",
+                WidgetPropertyCategory.Position, INPUT_LABEL_STACKING.stringValues(), INPUT_LABEL_STACKING.getDefault()));
+
     }
     
     public RGB getBackgroundFocusColor(){
@@ -60,5 +105,21 @@ public class TextInputStyledModel extends TextInputModel {
     
     public void setConfirmOnFocusLost(boolean confirm){
         setPropertyValue(PROP_CONFIRM_FOCUS_LOST, confirm);
+    }
+    
+    public String getInputLabelText(){
+        return (String)getCastedPropertyValue(PROP_INPUT_LABEL_TEXT);
+    }
+
+    public void setInputLabelText(String labelText){
+        setPropertyValue(PROP_INPUT_LABEL_TEXT, labelText);
+    }
+    
+    public void setText(String labelText, boolean fire){
+        getProperty(PROP_INPUT_LABEL_TEXT).setPropertyValue(labelText, fire);
+    }
+    
+    public INPUT_LABEL_STACKING getInputLabelStacking(){
+        return INPUT_LABEL_STACKING.values()[(int)getPropertyValue(PROP_INPUT_LABEL_STACKING)];
     }
 }
