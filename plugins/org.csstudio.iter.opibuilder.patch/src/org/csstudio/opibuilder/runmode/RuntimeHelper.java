a96c0520712c90a6968b9ede54a4d0bc5d0436c3
jbobnar <jaka.bobnar@cosylab.com>
2016-02-20 18:55:30 +0100

    Connectors and linking containers hacking.

--- plugins/org.csstudio.iter.opibuilder.patch/src/org/csstudio/opibuilder/runmode/RuntimeHelper.java
+++ plugins/org.csstudio.iter.opibuilder.patch/src/org/csstudio/opibuilder/runmode/RuntimeHelper.java
@@ -7,40 +7,41 @@
  ******************************************************************************/
 package org.csstudio.opibuilder.runmode;
 
-import org.csstudio.opibuilder.runmode.IOPIRuntime;
-import org.csstudio.opibuilder.runmode.OPIRunner;
-import org.csstudio.opibuilder.runmode.OPIRunnerPerspective;
-import org.csstudio.opibuilder.runmode.RunModeService.DisplayMode;
-import org.csstudio.opibuilder.runmode.RunnerInput;
 import org.csstudio.opibuilder.util.ErrorHandlerUtil;
 import org.csstudio.opibuilder.util.MacrosInput;
-import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.osgi.util.NLS;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.IPageListener;
 import org.eclipse.ui.IWorkbenchPage;
-import org.eclipse.ui.IWorkbenchPart;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.PartInitException;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.WorkbenchException;
-import org.eclipse.ui.handlers.HandlerUtil;
 
+/**
+ *
+ * <code>RuntimeHelper</code> provides convenience methods to display the opi. This methods replace community methods
+ * from the SingleSourceHelper.
+ *
+ * @author <a href="mailto:jaka.bobnar@cosylab.com">Jaka Bobnar</a>
+ *
+ */
 public final class RuntimeHelper {
 
     private static IWorkbenchWindow runWindow;
 
-    static IOPIRuntime getOPIRuntimeForEvent(ExecutionEvent event) {
-        IWorkbenchPart part = HandlerUtil.getActivePart(event);
-        return part instanceof IOPIRuntime ? (IOPIRuntime)part : null;
-    }
-
+    /**
+     * Opens the OPI in the main opi running window. The method does the same as was used before
+     * community replace the opi editor implementation with the opi view.
+     *
+     * @param path the path to the file to open
+     * @param input macros to use
+     */
     static void openOPIShell(IPath path, MacrosInput input) {
         try {
             if (runWindow == null) {
-                runWindow = PlatformUI.getWorkbench().openWorkbenchWindow(
-                        OPIRunnerPerspective.ID, null);
+                runWindow = PlatformUI.getWorkbench().openWorkbenchWindow(OPIRunnerPerspective.ID, null);
                 runWindow.addPageListener(new IPageListener() {
                     public void pageClosed(IWorkbenchPage page) {
                         runWindow = null;
@@ -61,14 +62,20 @@ public final class RuntimeHelper {
                 shell.setMinimized(false);
             shell.forceActive();
             shell.forceFocus();
-            openDisplay(page, new RunnerInput(path, null, input), DisplayMode.NEW_TAB);
+            openDisplay(page, new RunnerInput(path, null, input));
             shell.moveAbove(null);
         } catch (WorkbenchException e) {
             ErrorHandlerUtil.handleError(NLS.bind("Failed to open {0}", path), e);
         }
     }
 
-    static void openDisplay(IWorkbenchPage page, RunnerInput input, DisplayMode mode) {
+    /**
+     * Opens an opi file in a new editor.
+     *
+     * @param page the page in which to open the editor
+     * @param input the file to open
+     */
+    static void openDisplay(IWorkbenchPage page, RunnerInput input) {
         try {
             page.openEditor(input, OPIRunner.ID);
         } catch (PartInitException e) {
