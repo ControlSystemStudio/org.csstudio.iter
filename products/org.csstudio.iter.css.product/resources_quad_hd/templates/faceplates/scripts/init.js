c295df2621d8ed49b5af70e2ed9b003cd8e36ea3
utzeln <nadine.utzel@iter.org>
2016-04-28 13:29:17 +0000

    Standardization of input scripts in faceplates

--- products/org.csstudio.iter.css.product/resources_quad_hd/templates/faceplates/scripts/init.js
+++ products/org.csstudio.iter.css.product/resources_quad_hd/templates/faceplates/scripts/init.js
@@ -10,18 +10,59 @@ refer to the file ITER-LICENSE.TXT located in the top level directory
 of the distribution package.
 */
 
+/*
+V1.0 Feb 2016 - initialization of number when -1
+pvs[0]		: input field PV
+pvs[1]		: PV to be read to initialise the input field
+pvs[2]		: input field flag
+
+V2.0 June 2016 - support of number and text initialization via pvs[3] (default: number).
+	Executed when flag == NOK (no more on -1 value)
+*/
+
 importPackage(Packages.org.csstudio.opibuilder.scriptUtil);
 importPackage(Packages.org.csstudio.platform.data);
 
-var request_value = PVUtil.getDouble(pvs[0]);
-var request       = pvs[0];
-var init_value    = PVUtil.getDouble(pvs[1]);
-var flag          = pvs[2];
-var OK            = 1;
-var null_value    = -1;
+
+var	flag		= pvs[2],
+	flag_value	= PVUtil.getLong(flag);
+
+// Input flag enumeration
+var OK          = 1,
+	UNCHANGED	= 2,
+	SUBMITTED   = 3,
+	CANCELED    = 4,
+	ENDED       = 5,
+	NOK         = 0,
+	BADINPUT	= -1,
+	RANGEOVER	= -2,
+	RANGEUNDER	= -3;
+	
 
 // If none, set an initial value
-if (request_value == null_value) {
-	request.setValue(init_value);
+if (flag_value == NOK) {
+	
+	// Input type: number, text
+	var	input_type	= "number";	
+	try {
+		input_type	= PVUtil.getString(pvs[3]);
+	}
+	catch(err) {
+	// If input_type is not defined, a number is assumed
+	}
+		
+	var input		= pvs[0],
+		init		= pvs[1];
+	
+	// Depending of the input type
+	if (input_type == "number") {
+			var input_value	= PVUtil.getDouble(input);
+			var init_value	= PVUtil.getDouble(init);
+	} else if (input_type == "text") {
+			var input_value	= PVUtil.getString(input);
+			var init_value	= PVUtil.getString(init);
+	}
+	
+	input.setValue(init_value);
 	flag.setValue(OK);	
 }
--- products/org.csstudio.iter.css.product/resources_quad_hd/templates/faceplates/scripts/simul.js
+++ products/org.csstudio.iter.css.product/resources_quad_hd/templates/faceplates/scripts/simul.js
@@ -10,39 +10,63 @@ refer to the file ITER-LICENSE.TXT located in the top level directory
 of the distribution package.
 */
 
+/*
+V1.0 Feb 2016 - readback simulated when no I/O connected
+pvs[0]		: input PV
+pvs[1]		: readback PV
+pvs[2]		: input field flag
+
+V2.0 June 2016 - ...
+*/
+
 importPackage(Packages.org.csstudio.opibuilder.scriptUtil);
 importPackage(Packages.java.lang);
 
-var request    = PVUtil.getDouble(pvs[0]);
-var feedback   = pvs[1];
-var init_value = PVUtil.getDouble(pvs[1]);
-var status	   = PVUtil.getLong(pvs[2]);
-var Canceled   = 3;
+// Input flag enumeration
+var OK          = 1,
+	UNCHANGED	= 2,
+	SUBMITTED   = 3,
+	CANCELED    = 4,
+	ENDED       = 5,
+	NOK         = 0,
+	BADINPUT	= -1,
+	RANGEOVER	= -2,
+	RANGEUNDER	= -3;
+
+var	flag		= pvs[2],
+	flag_value	= PVUtil.getLong(flag);
+
+var input			= pvs[0],
+	input_value		= PVUtil.getDouble(input),
+	readback		= pvs[1], 
+	readback_value	= PVUtil.getDouble(readback);
+
+var init_value = PVUtil.getDouble(readback);
 
 runnable = {
 	run:function() {	
-		var i = (request < init_value) ? -1 : 1;
+		var i = (input_value < init_value) ? -1 : 1;
 		var position = init_value;
 		
-		//Cancel operation may change the request
+		//Cancel operation may change the input
 		//In this case, the submitted action is terminated
-		while (position != request) {
+		while (position != input_value) {
 			if(!display.isActive())
 				return;			
 			Thread.sleep(1000);
 			
-			//If request has been canceled, the simulation has to be undone
-			if (status == Canceled) {
-				i = (request < init_value) ? -1 : 1;
+			//If input has been canceled, the simulation has to be undone
+			if (flag_value == CANCELED) {
+				i = (input_value < init_value) ? -1 : 1;
 			}
 			position +=i;
-			feedback.setValue(position);
+			readback.setValue(position);
 		}
 	}	
 };		
 
 //The script can be triggered by a Cancel action
 //Requested value should be different from the initial one, otherwise nothing to do
-if (status != Canceled && request != init_value) {
+if (flag_value != CANCELED && input_value != init_value) {
 	new Thread(new Runnable(runnable)).start();
 }
