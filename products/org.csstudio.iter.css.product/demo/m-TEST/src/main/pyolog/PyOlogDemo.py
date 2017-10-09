b26e3469ed1314e9aebce2499a132ffd55a6554d
Borut Terpinc <borut.terpinc@cosylab.com>
2017-10-09 14:42:44 +0000

    PyOlogDemo now uses the configuration file

--- products/org.csstudio.iter.css.product/demo/m-TEST/src/main/pyolog/PyOlogDemo.py
+++ products/org.csstudio.iter.css.product/demo/m-TEST/src/main/pyolog/PyOlogDemo.py
@@ -4,20 +4,27 @@ Created on Nov 18, 2014
 @author: bobnarj
 
 13.9.2017 - Borut Terpinc
-Added the text update test 
+Added the text update test
+Client configuration file is read from the _conf.py
 '''
 
 from pyOlog import Logbook, LogEntry, OlogClient, Attachment
+from _conf import _conf
 from datetime import datetime
-import time, os
+import time, os, sys
 
 if __name__ == '__main__':
-    ologurl = 'http://localhost:8082/Olog'
-    ologUsername = 'anonymous'
-    ologPassword = '$anonymous'
+    def getDefaultConfig(arg):
+        if _conf.has_option('DEFAULT', arg):
+            return _conf.get('DEFAULT', arg)
+        else:
+            print('No python configuration for ' + arg + ' found. Please create ~/pyOlog.conf or /etc/pyOlog.conf. \n Exiting demo!')
+            sys.exit();
     
-    client = OlogClient(url=ologurl,username=ologUsername,password=ologPassword)
-    
+    ologurl = getDefaultConfig('ologurl')
+
+    client = OlogClient(url = ologurl ,username=getDefaultConfig('ologUsername'),password=getDefaultConfig('ologPassword'))
+
     ''' List all logbooks that currently exist in elog '''
     def listLogbooks():
         print('Retrieving the logbooks from ' + ologurl)
@@ -25,45 +32,45 @@ if __name__ == '__main__':
         print('  Logbooks:')
         for s in logs:
             print('    ' + s.getName())
-        
-    ''' Create a new entry containing a specific text in the logbook CODAC '''    
+
+    ''' Create a new entry containing a specific text in the logbook CODAC '''
     def createTextEntry():
         lb = Logbook(name='CODAC')
         print('Creating a text entry in the logbook ' + lb.getName())
         text = 'This is a demo python log entry created on ' + datetime.now().isoformat(' ')
         '''owner doesn't play any role here but it is required'''
-        testLog = LogEntry(text=text, owner='owner', logbooks=[lb]) 
+        testLog = LogEntry(text=text, owner='owner', logbooks=[lb])
         client.log(logEntry=testLog)
-    
-    ''' Create a new entry containing a specific text in all logbooks '''     
+
+    ''' Create a new entry containing a specific text in all logbooks '''
     def createTextEntryMultipleLogbooks():
         logs = client.listLogbooks()
         print('Creating a text entry in all logbooks')
         text = 'This is a demo python log entry created on ' + datetime.now().isoformat(' ')
-        testLog = LogEntry(text=text, owner='owner', logbooks=logs) 
+        testLog = LogEntry(text=text, owner='owner', logbooks=logs)
         client.log(logEntry=testLog)
-        
-    ''' Create a new entry in the logbook CODAC and use the first tag from the list of available tags '''    
+
+    ''' Create a new entry in the logbook CODAC and use the first tag from the list of available tags '''
     def createEntryWithTags():
         lb = Logbook(name='CODAC')
         tags = client.listTags()
-        print('Creating a text entry with a tag ' + tags[0].getName() + ' in the logbook ' + lb.getName())     
+        print('Creating a text entry with a tag ' + tags[0].getName() + ' in the logbook ' + lb.getName())
         text = 'This is a demo python log entry created on ' + datetime.now().isoformat(' ')
-        testLog = LogEntry(text=text, owner='owner', logbooks=[lb], tags=[tags[0]]) 
+        testLog = LogEntry(text=text, owner='owner', logbooks=[lb], tags=[tags[0]])
         client.log(logEntry=testLog)
-        
+
     ''' Create a new entry in logbook CODAC and attach an image to that entry '''
     def createEntryWithAttachment():
         lb = Logbook(name='CODAC')
-        print('Creating a text entry with an attached image in the logbook ' + lb.getName())     
+        print('Creating a text entry with an attached image in the logbook ' + lb.getName())
         text = 'This is a demo python log entry  wtih image attachment created on ' + datetime.now().isoformat(' ')
         pp = os.path.dirname(os.path.abspath(__file__))
         path = os.path.relpath('Desert.png', pp)
         image = Attachment(open(path,'rb'))
-        testLog = LogEntry(text=text, owner='owner', logbooks=[lb], attachments=[image]) 
+        testLog = LogEntry(text=text, owner='owner', logbooks=[lb], attachments=[image])
         client.log(logEntry=testLog)
-        
-    ''' Search for all entries (in all logbooks) that contain the word \'demo\' and have been made in the last hour'''    
+
+    ''' Search for all entries (in all logbooks) that contain the word \'demo\' and have been made in the last hour'''
     def searchForLastHourEntries():
         startTime = str(time.time()-3600).split('.')[0]
         endTime = str(time.time()).split('.')[0]
@@ -71,28 +78,29 @@ if __name__ == '__main__':
         logs = client.find(search='*demo*',start=startTime, end=endTime)
         for s in logs:
             print(s.getText())
-            
+
         print('Entries created in the last hour with tag \'Design\'')
-        logs = client.find(search='*',start=startTime, end=endTime, tag='Design')
+        logs = client.find(search='*demo*',start=startTime, end=endTime, tag='Design')
         for s in logs:
             print(s.getText())
 
-    '''Update log entry with image attachment '''        
+    '''Update log entry with image attachment '''
     def updateLogEntry():
         lb = Logbook(name='CODAC')
-        print('Creating a log entry which will be updated in logbook ' + lb.getName())         
+        print('Creating a log entry which will be updated in logbook ' + lb.getName())
         text = 'This is a demo python log entry which will be updated, created on ' + datetime.now().isoformat(' ')
-        testLog = LogEntry(text=text, owner='owner', logbooks=[lb]) 
+        testLog = LogEntry(text=text, owner='owner', logbooks=[lb])
         log = client.log(logEntry=testLog)
 
         textupdate = '<br>And this is update text to python log entry ' + datetime.now().isoformat(' ')
-        updatedLogEntry = LogEntry(text=text + textupdate, owner='owner', logbooks=[lb]) 
+        updatedLogEntry = LogEntry(text=text + textupdate, owner='owner', logbooks=[lb])
         updatedLog = client.update(log.getId(), updatedLogEntry)
         logEntries = client.find(search=text)
-        
+
 '''Here update methods are called. You can commnet out the desired methods '''
 '''
-listLogbooks()    
+
+listLogbooks()
 print('')
 createTextEntry()
 print('')
---
+++ products/org.csstudio.iter.css.product/demo/m-TEST/src/main/pyolog/_conf.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Internal module
+
+Used to read the pyOlog.conf file
+
+example file
+cat ~/pyOlog.conf
+[DEFAULT]
+url=http://localhost:8000/Olog
+"""
+
+def __loadConfig():
+    import os.path
+    import ConfigParser
+    dflt={'url':'http://localhost:8000/Olog'}
+    cf=ConfigParser.SafeConfigParser(defaults=dflt)
+    cf.read([
+        '/etc/pyOlog.conf',
+        os.path.expanduser('~/pyOlog.conf'),
+        'pyOlog.conf', 
+        '/opt/codac/lib/python/site-packages/pyOlog.conf'
+    ])
+    return cf
+
+_conf=__loadConfig()
