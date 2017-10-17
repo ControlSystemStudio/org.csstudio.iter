6d05f95eb5c1dad76ca402fbb768c3cca1fc9a8e
utzeln <nadine.utzel@iter.org>
2017-10-17 07:36:40 +0000

    New UTIL-S15 test case for multiple beast alarm root

---
+++ products/org.csstudio.iter.css.product/demo/m-TEST/src/main/run.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+function softIoc {
+    ######################################################################
+    # Run the IOCs
+    ######################################################################
+    
+    echo -e "\n\tStart the IOCs...\n"
+    
+    tab=" --tab"
+    options=" --profile='default'"
+    
+    cmds[1]="bash -c 'softIoc -d ./epics/CTRL-SUPApp/Db/PSH0-CTRL-SUP-CSS.db'"
+    
+    cmds[2]="bash -c 'softIoc -d ./epics/CTRL-SUPApp/Db/PSH0-CTRL-SUP-BOY.db'"
+    
+    cmds[3]="bash -c 'softIoc -d ./epics/CTRL-SUPApp/Db/PSH0-CTRL-SUP-BEAS.db'"
+    
+    cmds[4]="bash -c 'softIoc -d ./epics/CTRL-SUPApp/Db/PSH0-CTRL-SUP-BEAU.db'"
+    
+    cmds[5]="bash -c 'softIoc -d ./epics/UTIL-S15App/Db/PSH0-UTIL-S15-0000.db'"
+     
+    for i in {1..5}; do
+        options+=($tab -e "\"${cmds[i]}\"")
+    done
+}
+
+function alarm {
+    ######################################################################
+    # Configure and start the alarm system
+    ######################################################################
+    
+    echo -e "\n\tConfigure and start the alarm system...\n"
+    
+    tab=" --tab"
+    options=" --profile='default'"
+    
+    cmds[1]="bash -c 'alarm-configtool -root demo -import -file ./beast/CTRL-beast.xml ; alarm-server -root demo'"
+    
+    for i in {1..1}; do
+        options+=($tab -e "\"${cmds[i]}\"")
+    done
+}
+
+function archive {
+    ######################################################################
+    # Configure and start the archive system
+    ######################################################################
+    
+    echo -e "\n\tConfigure and start the archive system...\n"
+    
+    tab=" --tab"
+    options=" --profile='default'"
+    
+    cmds[1]="bash -c 'archive-configtool -engine demo -port 5812 -import -config ./beauty/CTRL-beauty.xml -replace_engine; archive-engine -engine demo -port 5812'"
+    
+    for i in {1..1}; do
+        options+=($tab -e "\"${cmds[i]}\"")
+    done
+}
+
+date
+
+softIoc
+
+alarm
+
+archive
+
+CMD="gnome-terminal "${options[@]}""
+echo -e "$CMD"
+eval "$CMD"
+
+exit 0
